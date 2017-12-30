7eb2119785e8365123c854403eb2cd8a9fa4323f
김진선 <jinseonkim@gimjinseon-ui-MacBook-Pro.local>
2017-12-30 16:28:39 +0900

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by 김진선 on 2017. 12. 30..
+//  Copyright © 2017년 JinseonKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
